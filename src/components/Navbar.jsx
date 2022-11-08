68e07a96cc79f1dde54ebbf268d3dd0611a2be47
Mushfikunnabi Nijhum <m.nijhum0@gmail.com>
2022-11-08 10:59:33 +0600

    folder structered

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="w-full flex py-6 justify-between items-center navbar"></nav>
+  );
+};
+
+export default Navbar;
